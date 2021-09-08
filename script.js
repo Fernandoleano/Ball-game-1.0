31fb8db112ef003587cf2ba3b1787369b6d491a6
Fernando Leano <fernandoleano4@gmail.com>
2021-09-08 17:47:52 -0400

    Changes

--- script.js
+++ script.js
@@ -3,5 +3,12 @@
     Developer
 */
 
-let canvas = document.createElement('gameCanvas');
-let context = canvas.getContext('2d');
+let canvas = document.getElementById("gameCanvas"); // use getElementById not createElement
+let context = canvas.getContext("2d");
+
+context.beginPath();
+// Making a square (To test it out)
+context.rect(20, 40, 50, 50);
+context.fillStyle = "#FF0000";
+context.fill();
+context.closePath();
